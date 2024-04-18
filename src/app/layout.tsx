2edfddfff6db403e748fb1473720d5c5659869d9
Saltico <matias.castillo@sansano.usm.cl>
2024-04-17 23:16:29 -0400

    [new]: Title changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Pokedex Examedi",
-  description: "Prueba técnica para Examedi.",
+  title: "Pokedex by Matías Castillo",
+  description: "Prueba ténica de Next.js",
 };
 
 export default function RootLayout({
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <main className="container">
-            <h1 style={{color: 'black', textAlign: 'center', fontSize: '150%', fontWeight: '600'}}>ExamediDex</h1>    
+            <h1 style={{color: 'black', textAlign: 'center', fontSize: '150%', fontWeight: '600'}}>Pokedex</h1>    
             <div className="wrapper">
               {children}
             </div>
